88764d776d4cd25092b3a49f2e5e0fd1a6fe0b14
Govardhan K G <govi.reddy420@gmail.com>
2025-12-14 15:18:06 +0530

    added xreadgroup func

--- apps/worker/index.ts
+++ apps/worker/index.ts
@@ -1,1 +1,20 @@
-import {prisma} from "store/cleint"
+import {xACK,xReadGroup} from "redisstream/client"
+const REGION_ID=process.env.REGION_ID!
+const WORKER_ID=process.env.WORKER_ID!
+if (!REGION_ID) {
+	throw new Error("Region not provided");
+}
+if (!WORKER_ID) {
+	throw new Error("woorker not provided");
+}
+async function main() {
+	while(1){
+		// read from the stream 
+		const response=xReadGroup(REGION_ID,WORKER_ID)
+
+		//  ack back to queue
+		xACK(REGION_ID,"a")
+	}
+}
+
+main()
--- packages/redisstream/index.ts
+++ packages/redisstream/index.ts
@@ -2,9 +2,11 @@ import { createClient } from "redis";
 const client=await createClient()
 	.on("error",(err)=>{console.log("redis stream error",err)})
 	.connect()
+
+	const STREAM_NAME="betteruptime:website"
 type websiteEvent={url:string,id:string}
 async function  xADD({url,id}:websiteEvent) {
-	await client.xAdd("betteruptime:website","*",{
+	await client.xAdd(STREAM_NAME,"*",{
 		url,id
 	})
 }
@@ -16,4 +18,22 @@ export async function xADDBulk(websites:websiteEvent[]) {
 			id:websites[i]?.id as string
 		})
 	}
+}
+
+
+export async function xReadGroup(consumerGroup:string,workerId:string):Promise<any> {
+const res=	await client.xReadGroup(
+		consumerGroup,workerId,{
+			key:STREAM_NAME,
+			id:'>'
+		},{
+			'COUNT':5
+		}
+	)
+	return res;
+}
+
+
+export async function xACK(consumerGroup:string,streamId:string) {
+	await client.xAck(STREAM_NAME,consumerGroup,streamId)
 }
